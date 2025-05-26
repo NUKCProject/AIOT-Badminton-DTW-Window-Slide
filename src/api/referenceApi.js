80cba90e5f91e99bdd93ba8f06f285147eb626ab
Nero <sheep5168947@gmail.com>
2025-05-27 01:02:40 +0800

    fix: update API baseURL and increase timeout for reference API calls

--- src/api/referenceApi.js
+++ src/api/referenceApi.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 // 這邊設定你的後端BaseURL
 const apiClient = axios.create({
-  // baseURL: 'https://badminton-457613.de.r.appspot.com',
-  baseURL: 'http://127.0.0.1:8000',
-  timeout: 60000,
+  baseURL: 'https://badminton-461016.de.r.appspot.com',
+  // baseURL: 'http://127.0.0.1:8000',
+  timeout: 600000,
 })
 
 // 抓小段waveform
